31212a765ddeb60df3d2570c0d98ebdc7ab226de
Alex Ryder <ajryder31@gmail.com>
2024-05-29 21:01:47 -0600

    added stuff to schema folder as well as utils and the server

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,37 @@
+const { User, Post } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+
+    },
+
+    Mustaions: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+          },
+          login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+    },
+
+
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    password: String
+    email: String
+    post: [Post]!
+  }
+
+  type Post {
+    _id: ID
+    img: String
+    createdAt: String
+    user: String
+    reactions: [Reactions]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    post(username: String): [Post]
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    // addThought(thoughtText: String!): Thought
+    // addComment(thoughtId: ID!, commentText: String!): Thought
+    // removeThought(thoughtId: ID!): Thought
+    // removeComment(thoughtId: ID!, commentId: ID!): Thought
+  }
+`;
+
+module.exports = typeDefs;
