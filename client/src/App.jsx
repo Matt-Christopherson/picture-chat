a8b75aad67dae362623ffbb4045f57c023394a6f
parkerrappleye <parker.rappleye1@gmail.com>
2024-06-05 20:34:11 -0600

    connected login/signup frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,35 @@
 import { useState } from 'react';
 import './App.css';
 import Canvas from './components/Canvas';
+import SignUp from './components/Signup';
+import Login from './components/Login';
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
-	return (
-		<>
-			<div className="App">
-				<header className="App-header">
-					<h1>Picture Chat</h1>
-					<nav>
-						<ul>
-							<li><a href="#logout">logout</a></li>
-							<li><a href="#signup">signup</a></li>
-							<li><a href="#login">login</a></li>
-						</ul>
-            		</nav>
-				</header>
-				<Canvas />
-			</div>
-		</>
-	);
+  const toggleSignUp = () => setShowSignUp(!showSignUp);
+  const toggleLogin = () => setShowLogin(!showLogin);
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Picture Chat</h1>
+          <nav>
+            <ul>
+              <li><a href="#logout">logout</a></li>
+              <li><a href="#signup" onClick={toggleSignUp}>signup</a></li>
+              <li><a href="#login" onClick={toggleLogin}>login</a></li>
+            </ul>
+          </nav>
+        </header>
+        <Canvas />
+        {showSignUp && <SignUp onClose={toggleSignUp} />}
+        {showLogin && <Login onClose={toggleLogin} />}
+      </div>
+    </>
+  );
 }
 
 export default App;
