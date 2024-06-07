02dd13b598847679eea63a76f649f2f3ad2d0730
Tyler Sanford <tylersanford1997@gmail.com>
2024-06-06 21:13:27 -0600

    Connecting front to back

---
+++ client/src/utils/queries.js
@@ -0,0 +1,48 @@
+import { gql } from '@apollo/client';
+// Queries
+export const GET_USERS = gql`
+  query users {
+    users {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const GET_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const GET_POSTS_BY_USER = gql`
+  query post($username: String!) {
+    post(username: $username) {
+      _id
+      postImage
+      user
+      createdAt
+      reactions {
+        _id
+        reactionBody
+        username
+        createdAt
+      }
+    }
+  }
+`;
+
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+    }
+  }
+`;
