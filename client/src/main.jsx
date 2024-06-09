538977ae6a5e440bb659c7361ca0166769eb90ab
unknown <Connor.Spendlove44@gmail.com>
2024-06-08 19:16:50 -0600

    changed main.jsx to handle apollo provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import App from './App.jsx';
+import './index.css';
+
+const client = new ApolloClient({
+  uri: 'https://your-graphql-endpoint.com/graphql', // replace with your GraphQL endpoint
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-)
+);
