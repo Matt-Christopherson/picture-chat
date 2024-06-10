7cfc867a2b46b0929ab222325ca646bc6061cf2d
Alex Ryder <ajryder31@gmail.com>
2024-06-10 17:12:03 -0600

    updateddb

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,10 +2,16 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from 'mongoose';
 
 export default async function connect(){
-  const mongoServer = await MongoMemoryServer.create();
-  const mongoUri = mongoServer.getUri();
+  // const mongoServer = await MongoMemoryServer.create();
+  // const mongoUri = mongoServer.getUri();
 
-  await mongoose.connect(mongoUri, { dbName: "picure-chat"});
-  console.log(`MongoDB connected to ${mongoUri}`);
+  // await mongoose.connect(mongoUri, { dbName: "picture-chat"});
+  // console.log(`MongoDB connected to ${mongoUri}`);
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/picture-chat', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 };
+
+
 // config.js
